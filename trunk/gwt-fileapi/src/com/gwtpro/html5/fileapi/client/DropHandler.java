b95ef24e204c8ad4ccb5858754c079cea7b96554
tony.benbrahim <tony.benbrahim@796d46be-4c0a-1118-f848-ed3be2313fec>
2010-05-16 21:48:48 +0000

    added javadoc for file drop API

--- trunk/gwt-fileapi/src/com/gwtpro/html5/fileapi/client/DropHandler.java
+++ trunk/gwt-fileapi/src/com/gwtpro/html5/fileapi/client/DropHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Abed Tony BenBrahim
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.gwtpro.html5.fileapi.client;
 
 import com.google.gwt.core.client.JsArray;
@@ -9,21 +24,45 @@ import com.gwtpro.html5.fileapi.client.events.FileEvent;
 import com.gwtpro.html5.fileapi.client.events.HasFileEvents;
 import com.gwtpro.html5.fileapi.client.events.FileEvent.FileEventHandler;
 
-public class DropHandler implements HasFileEvents{
+/**
+ * Wires a widget or HTML element for dropping files from the operating system
+ * 
+ * @author Abed Tony BenBrahim
+ * 
+ */
+public class DropHandler implements HasFileEvents {
 
     private JsArray<File> files;
     private final HandlerManager handlerManager;
 
+    /**
+     * constructs a drop handler for an HTML element
+     * 
+     * @param element
+     *            the element which will be wired to detect file drops
+     */
     public DropHandler(Element element) {
-        this.handlerManager=new HandlerManager(this);
+        this.handlerManager = new HandlerManager(this);
         bindTo(element);
     }
 
+    /**
+     * constructs a drop handle for a GWT widget
+     * 
+     * @param widget
+     *            the widget which will be wired to detect file drops
+     */
     public DropHandler(Widget widget) {
-        this.handlerManager=new HandlerManager(this);
+        this.handlerManager = new HandlerManager(this);
         bindTo(widget.getElement());
     }
 
+    /**
+     * register a handler for file drop events
+     * 
+     * @param handler
+     *            the handler for file drop events
+     */
     @Override
     public HandlerRegistration addFileEventHandler(FileEventHandler handler) {
         return this.handlerManager.addHandler(FileEvent.TYPE, handler);
@@ -52,5 +91,4 @@ public class DropHandler implements HasFileEvents{
     protected void filesDropped() {
         this.handlerManager.fireEvent(new FileEvent(this.files));
     }
-
 }
