e472eecd862df5b400a2872ce776132c72dd6bf7
tony.benbrahim <tony.benbrahim@796d46be-4c0a-1118-f848-ed3be2313fec>
2010-05-18 02:32:59 +0000

    refactored file upload to mirror RequestBuilder with new UploadRequestBuilder class

--- trunk/gwt-fileapi/src/com/gwtpro/html5/fileapi/client/samples/SimpleDropUploadDemo.java
+++ trunk/gwt-fileapi/src/com/gwtpro/html5/fileapi/client/samples/SimpleDropUploadDemo.java
@@ -18,85 +18,75 @@ package com.gwtpro.html5.fileapi.client.samples;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.gwtpro.html5.fileapi.client.DropHandler;
 import com.gwtpro.html5.fileapi.client.File;
-import com.gwtpro.html5.fileapi.client.FileUploader;
+import com.gwtpro.html5.fileapi.client.UploadRequest;
+import com.gwtpro.html5.fileapi.client.UploadRequestBuilder;
+import com.gwtpro.html5.fileapi.client.UploadRequestCallback;
 import com.gwtpro.html5.fileapi.client.events.FileEvent;
-import com.gwtpro.html5.fileapi.client.events.FileUploadDoneEvent;
-import com.gwtpro.html5.fileapi.client.events.FileUploadProgressEvent;
 import com.gwtpro.html5.fileapi.client.events.FileEvent.FileEventHandler;
-import com.gwtpro.html5.fileapi.client.events.FileUploadDoneEvent.FileUploadDoneEventHandler;
-import com.gwtpro.html5.fileapi.client.events.FileUploadProgressEvent.FileUploadProgressEventHandler;
 
 public class SimpleDropUploadDemo implements EntryPoint {
 
     private int currentFile;
     private JsArray<File> files;
-    private FileUploader fileUploader;
+    private UploadRequestBuilder fileUploader;
+    private Grid table;
 
     @Override
     public void onModuleLoad() {
         RootPanel rootPanel = RootPanel.get();
         DropHandler dropHandler = new DropHandler(rootPanel);
-        final Grid table = new Grid();
-        RootPanel.get("table").add(table);
-        this.fileUploader = new FileUploader();
-        this.fileUploader.setUrl(GWT.getModuleBaseURL() + "upload-test");
-        this.fileUploader
-                .addFileUploadDoneEventHandler(new FileUploadDoneEventHandler() {
+        this.table = new Grid();
+        RootPanel.get("table").add(this.table);
+        String url = GWT.getModuleBaseURL() + "upload-test";
+        this.fileUploader = new UploadRequestBuilder(url);
+        this.fileUploader.setCallback(new UploadRequestCallback() {
 
-                    @Override
-                    public void onDone(FileUploadDoneEvent event) {
-                        if (event.isSuccessful()) {
-                            table.setText(
-                                    SimpleDropUploadDemo.this.currentFile + 1,
-                                    2, "success: " + event.getResponse());
-                        } else if (event.getResponseCode() == 0) {
-                            table.setText(
-                                    SimpleDropUploadDemo.this.currentFile + 1,
-                                    2, "aborted");
-                        } else {
-                            table.setText(
-                                    SimpleDropUploadDemo.this.currentFile + 1,
-                                    2, "failed: " + event.getResponseCode()
-                                            + " " + event.getResponse());
-                        }
-                        uploadNextFile(SimpleDropUploadDemo.this.currentFile + 1);
-                    }
-                });
-        this.fileUploader
-                .addFileUploadProgressEventHandler(new FileUploadProgressEventHandler() {
+            @Override
+            public void onError(UploadRequest request, Throwable exception) {
+                SimpleDropUploadDemo.this.table.setText(SimpleDropUploadDemo.this.currentFile + 1, 2,
+                        "failed: " + exception.getMessage());
+                uploadNextFile(SimpleDropUploadDemo.this.currentFile + 1);
+            }
 
-                    @Override
-                    public void onProgress(FileUploadProgressEvent event) {
-                        // GWT.log(event.getProgress()+"",null);
-                        // table.setText(SimpleDropUploadDemo.this.currentFile+1,
-                        // 2,
-                        // (event.getProgress()*100/event.getFile().getFileSize())+"%");
-                        table.setText(
-                                SimpleDropUploadDemo.this.currentFile + 1, 2,
-                                event.getProgress() + "");
-                    }
-                });
+            @Override
+            public void onResponseReceived(UploadRequest request,
+                    Response response) {
+                SimpleDropUploadDemo.this.table.setText(SimpleDropUploadDemo.this.currentFile + 1, 2,
+                        "success: " + response.getText());
+                uploadNextFile(SimpleDropUploadDemo.this.currentFile + 1);
+            }
+
+            @Override
+            public void onUploadProgress(UploadRequest request,
+                    int bytesUploaded) {
+                SimpleDropUploadDemo.this.table.setText(SimpleDropUploadDemo.this.currentFile + 1, 2,
+                        bytesUploaded + "");
+            }
+
+        });
         dropHandler.addFileEventHandler(new FileEventHandler() {
 
             @Override
             public void onFiles(FileEvent event) {
                 SimpleDropUploadDemo.this.files = event.getFiles();
-                table.clear();
-                table.resize(SimpleDropUploadDemo.this.files.length() + 1, 3);
-                table.setText(0, 0, "File name");
-                table.setText(0, 1, "File size");
-                table.setText(0, 2, "Progress");
+                SimpleDropUploadDemo.this.table.clear();
+                SimpleDropUploadDemo.this.table.resize(SimpleDropUploadDemo.this.files.length() + 1, 3);
+                SimpleDropUploadDemo.this.table.setText(0, 0, "File name");
+                SimpleDropUploadDemo.this.table.setText(0, 1, "File size");
+                SimpleDropUploadDemo.this.table.setText(0, 2, "Progress");
                 for (int i = 0; i < SimpleDropUploadDemo.this.files.length(); ++i) {
-                    table.setText(i + 1, 0, SimpleDropUploadDemo.this.files
+                    SimpleDropUploadDemo.this.table.setText(i + 1, 0, SimpleDropUploadDemo.this.files
                             .get(i).getFileName());
-                    table.setText(i + 1, 1, SimpleDropUploadDemo.this.files
+                    SimpleDropUploadDemo.this.table.setText(i + 1, 1, SimpleDropUploadDemo.this.files
                             .get(i).getFileSize()
                             + "");
-                    table.setText(i + 1, 2, "");
+                    SimpleDropUploadDemo.this.table.setText(i + 1, 2, "");
                 }
                 uploadNextFile(0);
             }
@@ -106,8 +96,13 @@ public class SimpleDropUploadDemo implements EntryPoint {
     private void uploadNextFile(int index) {
         SimpleDropUploadDemo.this.currentFile = index;
         if (index < this.files.length()) {
-            this.fileUploader.sendFile(this.files
-                    .get(SimpleDropUploadDemo.this.currentFile));
+            try {
+                this.fileUploader.sendFile(this.files
+                        .get(SimpleDropUploadDemo.this.currentFile));
+            } catch (RequestException e) {
+                this.table.setText(index + 1, 2, "failed: " + e.getMessage());
+                uploadNextFile(index + 1);
+            }
         }
     }
 }
